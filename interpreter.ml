faaaef6f3e78c34589d70ee31f11a6c516695123
EnchoGenchev <esgenchev@gmail.com>
2025-10-17 15:53:59 -0500

    commented and cleaned up

--- interpreter.ml
+++ interpreter.ml
@@ -1,4 +1,4 @@
-(* Value type representing all stack values *)
+(*value type representing all stack values *)
 type value =
   | Int of int
   | Bool of bool
@@ -7,7 +7,7 @@ type value =
   | Error
   | Unit
 
-(* String representation for output/println, as specified *)
+(*string representation for output/println*)
 let value_to_string = function
   | Int n -> string_of_int n
   | Bool true -> ":true:"
@@ -17,112 +17,151 @@ let value_to_string = function
   | Error -> ":error:"
   | Unit -> ":unit:"
 
-(* Helper: strip surrounding quotes from a string *)
-let strip_quotes s =
-  let len = String.length s in
-  if len >= 2 && s.[0] = '"' && s.[len - 1] = '"'
-  then String.sub s 1 (len - 2)
-  else s
+(*strip surrounding quotes from a string*)
+let strip_quotes str =
+  let len = String.length str in
+  if len >= 2 && str.[0] = '"' && str.[len - 1] = '"'
+  then String.sub str 1 (len - 2) (*gets substring between first and last index, aka excludes quotes*)
+  else str (*if doesn't have quotes just return the string*)
 
-(* Check if a string is a valid integer, return option *)
-let parse_int_opt s =
-  try Some (int_of_string s) with _ -> None
+(*check if a string is a valid integer, return option *)
+let parse_int_opt str =
+  try Some (int_of_string str) with _ -> None (*if function doesn't work returns one*)
 
-let is_valid_name s =
-  let len = String.length s in
+let is_valid_name str =
+  let len = String.length str in
   if len = 0 then false
-  else
+  else 
+    (*functions for checking name criteria*)
     let is_letter c = ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') in
     let is_digit c = '0' <= c && c <= '9' in
     let is_underscore c = c = '_' in
-    (is_letter s.[0] || is_underscore s.[0]) &&
-    String.for_all (fun c -> is_letter c || is_digit c || is_underscore c) s
+    (*making sure string starts with a letter or underscore then making sure that's all there is*)
+    (is_letter str.[0] || is_underscore str.[0]) &&
+    (*string.for_all takes the functions along with the string as arguments
+    all the functions return booleans so final return will also be boolean*)
+    String.for_all (fun c -> is_letter c || is_digit c || is_underscore c) str 
 
-let parse_const s =
-  match s with
+let parse str =
+  match str with
+    (*returns value types to be pushed to stack*)
     | ":true:" -> Some (Bool true)
     | ":false:" -> Some (Bool false)
     | ":error:" -> Some Error
     | ":unit:" -> Some Unit
-    | _ when String.length s >= 2 && s.[0] = '"' && s.[String.length s-1] = '"' ->
-        Some (Str (strip_quotes s))
-    | _ ->
-      (match parse_int_opt s with
-       | Some n -> Some (Int n)
-       | None ->
-         if is_valid_name s then Some (Name s)
-         else None)
+    | _ -> 
+      if String.length str >= 2 && str.[0] = '"' && str.[String.length str - 1] = '"' then
+      Some (Str (strip_quotes str)) (*returns string without quotes*)
+      else
+      match parse_int_opt str with (*checking if it's an int*)
+      | Some n -> Some (Int n) 
+      | None -> (*if not then test if it's a valid name*)
+          if is_valid_name str 
+            then Some (Name str)
+          else None (*if not then it is not a valid value and returns an error in push*)
 
-let push_const stack s =
-  match parse_const s with
-  | Some v -> v :: stack
+let push stack str =
+  match parse str with (*parsing the string*)
+  | Some value -> value :: stack (*adding a value to the top of the stack*)
   | None -> Error :: stack
 
 let pop stack =
   match stack with
-  | [] -> [Error]
-  | _::tl -> tl
+  | [] -> [Error] (*if stack empty then there's nothing to pop*)
+  | _::tl -> tl (*if not empty then just return tail of stack*)
 
-let binop stack f =
+let add stack =
   match stack with
-  | Int y :: Int x :: tl -> (Int (f x y)) :: tl
-  | v1 :: v2 :: tl -> v2 :: v1 :: Error :: tl
+  (*if top two values are integers*)
+  | Int y :: Int x :: tl -> Int (x + y) :: tl 
+  (*top two values are not integers*)
+  | v1 :: v2 :: tl -> Error :: v1 :: v2 :: tl (*preserves stack but pushes error*)
+  (*only one element in stack*)
+  | [v] -> Error :: v :: [] (*pushes Error to the stack after the element*)
+  (*empty stack*)
+  | [] -> [Error] (*pushes error to the stack*)
+
+let sub stack = (*same as addition but with - symbol instead*)
+  match stack with
+  (*top two values are integers*)
+  | Int y :: Int x :: tl -> Int (x - y) :: tl 
+  (*top two values are not integers*)
+  | v1 :: v2 :: tl -> Error :: v1 :: v2  :: tl
+  (*only one element in stack*)
   | [v] -> v :: Error :: []
+  (*empty stack*)
   | [] -> [Error]
 
-let add stack = binop stack ( + )
-let sub stack = binop stack ( - )
-let mult stack = binop stack ( * )
-let div stack =
+let mult stack = (*same as add and sub but with * symbol*)
   match stack with
-  | Int 0 :: Int x :: tl -> Int x :: Int 0 :: Error :: tl
-  | Int y :: Int x :: tl -> Int (x / y) :: tl
-  | v1 :: v2 :: tl -> v2 :: v1 :: Error :: tl
+  (*top two values are integers*)
+  | Int y :: Int x :: tl -> Int (x * y) :: tl
+  (*top two values are not integers*)
+  | v1 :: v2 :: tl -> Error  :: v1 :: v2 :: tl
+  (*only one element in stack*)
   | [v] -> v :: Error :: []
+  (*empty stack*)
   | [] -> [Error]
 
-let rem stack =
+let div stack = (*like add, sub, and mult but with extra condition*)
   match stack with
-  | Int 0 :: Int x :: tl -> Int x :: Int 0 :: Error :: tl
+  (*division by 0*)
+  | Int 0 :: Int x :: tl -> Error :: Int x :: Int 0 :: tl 
+  (*top two values are integers*)
+  | Int y :: Int x :: tl -> Int (x / y) :: tl (*pushes x/y to the stack (rounds to nearest int)*)
+  (*top two values are not integers*)
+  | v1 :: v2 :: tl ->  Error :: v1 :: v2 :: tl
+  (*only one element in stack*)
+  | [v] -> v :: Error :: []
+  (*empty stack*)
+  | [] -> [Error]
+
+
+let rem stack = (*like add, sub, and mult but using mod*)
+  match stack with 
+  (*0 mod anything is not valid because it's like division by 0*)
+  | Int 0 :: Int x :: tl -> Error :: Int 0 :: Int x ::  tl
+   (*top two values are integers*)
   | Int y :: Int x :: tl -> Int (x mod y) :: tl
-  | v1 :: v2 :: tl -> v2 :: v1 :: Error :: tl
+  (*top two values are not integers*)
+  | v1 :: v2 :: tl -> Error ::  v1 :: v2 :: tl
+  (*only one element in stack*)
   | [v] -> v :: Error :: []
+  (*empty stack*)
   | [] -> [Error]
 
 let sign stack =
   match stack with
-  | Int x :: tl -> Int (if x = 0 then 0 else -x) :: tl
-  | v :: tl -> v :: Error :: tl
-  | [] -> [Error]
+  | Int x :: tl -> Int (if x = 0 then 0 else -x) :: tl (*0 doesn't have a sign otherwise push -x*)
+  | value :: tl -> Error :: value :: tl (*if not an integer*)
+  | [] -> [Error] (*if stack empty*)
 
 let swap stack =
   match stack with
   | v1 :: v2 :: tl -> v2 :: v1 :: tl
+  (*can't swap if there's less than 2 values in the stack*)
   | [v] -> v :: Error :: []
   | [] -> [Error]
 
 let to_string_stack stack =
   match stack with
-  | v :: tl -> Str (value_to_string v) :: tl
-  | [] -> [Error]
+  | value :: tl -> Str (value_to_string value) :: tl
+  | [] -> [Error] (*can't convert nothing to a string*)
 
-let println stack out_chan =
+let println stack out_file =
   match stack with
-  | v :: tl ->
-    output_string out_chan (value_to_string v ^ "\n");
-    tl
-  | [] ->
+  | value :: tl -> output_string out_file (value_to_string value ^ "\n");
+             tl (*return the rest of the stack*)
+  | [] -> (*stack empty so nothing to print*)
     [Error]
 
-(* Interpret a single command, returns new stack *)
-let interpret_command (stack : value list) (cmd : string) (out_chan : out_channel) : value list =
-  let cmd = String.trim cmd in
-  if String.length cmd = 0 then stack
+(*interpret a single command, returns new stack *)
+let interpret_command (stack : value list) (cmd : string) (out_file : out_channel) : value list =
+  if String.length cmd = 0 then stack (*return the stack once there are no more commands*)
   else
-    let words = String.split_on_char ' ' cmd in
+    let words = String.split_on_char ' ' cmd in (*splits words and puts in string list*)
     match words with
-    | ["push"; arg] ->
-      push_const stack arg
+    | ["push"; arg] -> push stack arg 
     | ["pop"] -> pop stack
     | ["add"] -> add stack
     | ["sub"] -> sub stack
@@ -132,35 +171,37 @@ let interpret_command (stack : value list) (cmd : string) (out_chan : out_channe
     | ["sign"] -> sign stack
     | ["swap"] -> swap stack
     | ["toString"] -> to_string_stack stack
-    | ["println"] -> println stack out_chan
-    (* allow choose push with quoted string arg with spaces: push "hello world" *)
-    | "push"::rest when List.length rest > 0 ->
-      let arg = String.concat " " rest in
-      push_const stack arg
-    | _ -> stack  (* ignore unknown/empty/quit here *)
+    | ["println"] -> println stack out_file
+    | "push" :: rest -> let arg = String.concat " " rest in push stack arg (*for strings with spaces*)
+    | _ -> stack
     
 
-(* Main interpreter function *)
+(*main function *)
 let interpreter ((input : string), (output : string)) : unit =
-  let in_chan = open_in input in
-  let out_chan = open_out output in
+  let in_file = open_in input in
+  let out_file = open_out output in
+
   let rec read_lines acc =
-    try read_lines (input_line in_chan :: acc) with End_of_file -> List.rev acc
+    (*prepend lines recursively until reach the end of the file*)
+    try read_lines (input_line in_file :: acc) 
+    with End_of_file -> List.rev acc (*reverse cuz prepended*)
   in
   let lines = read_lines [] in
+
   let rec process stack commands =
     match commands with
     | [] -> stack
     | cmd :: rest ->
-      let stripped = String.trim cmd in
+      let stripped = String.trim cmd in  (*trim whitespace and newlines so it reads correctly*)
       if stripped = "quit" then stack
       else
-        let s = interpret_command stack stripped out_chan in
+        let s = interpret_command stack stripped out_file in
         process s rest
   in
-  ignore (process [] lines);
-  close_in in_chan;
-  close_out out_chan
+
+  ignore(process [] lines); (*don't need the stack anymore*)
+  close_in in_file;
+  close_out out_file
 ;;
 
-interpreter ("input10.txt", "output10.txt");;
+interpreter ("input5.txt", "output5.txt");;
